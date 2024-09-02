b73197eec33ec5e914dfde88bb374363dc4dbd0d
ArlieT <torresarlie22@gmail.com>
2024-09-02 18:02:23 +0800

    fix: resume.pdf

--- app/(pdf)/arlie_torres.pdf/page.tsx
+++ app/(pdf)/arlie_torres.pdf/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 export default function Page() {
-  <div className="absolute inset-0 h-full w-full">
-    <iframe
-      title="arlie_torres resume pdf"
-      src="/pdf/arlie_torres.pdf"
-      width="100%"
-      height="100%"
-    />
-  </div>;
+  return (
+    <div className="absolute inset-0 h-full w-full">
+      <iframe
+        title="arlie_torres resume pdf"
+        src="/pdf/arlie_torres.pdf"
+        width="100%"
+        height="100%"
+      />
+    </div>
+  );
 }
