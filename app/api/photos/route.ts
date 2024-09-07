531e612a08fc2a90584fe2f87585981ccc0cf157
ArlieT <torresarlie22@gmail.com>
2024-09-08 05:49:41 +0800

    fix: prod building

--- app/api/photos/route.ts
+++ app/api/photos/route.ts
@@ -7,10 +7,9 @@ export async function GET() {
   const dir = path.join(process.cwd(), 'public/images/photography');
 
   try {
-    files = fs.readdirSync(dir).filter(
-      (file) => /\.(jpg|jpeg|png|gif|webp)$/i.test(file),
-      // Filter only image files (adjust extensions as needed)
-    );
+    files = fs
+      .readdirSync(dir)
+      .filter((file) => /\.(jpg|jpeg|png|gif|webp)$/i.test(file));
 
     const photos = await prisma.photos.findMany({
       orderBy: {
@@ -23,7 +22,10 @@ export async function GET() {
       status: 200,
     });
   } catch (error) {
-    return Response.json({ message: error }, { status: 500 });
+    console.error('GET error: ', error);
+    return new Response(JSON.stringify({ message: String(error) }), {
+      status: 500,
+    });
   }
 }
 
@@ -33,10 +35,10 @@ export async function PUT(request: Request) {
     const id = Number(number);
     console.log({ status });
 
-    if (number === null || Number.isNaN(id)) {
+    if (!id || Number.isNaN(id)) {
       return Response.json(
         { message: 'No id provided or not a number' },
-        { status: 500 },
+        { status: 400 },
       );
     }
     const photo = await prisma.photos.update({
@@ -45,8 +47,6 @@ export async function PUT(request: Request) {
         count: status === 'increment' ? { increment: 1 } : { decrement: 1 },
       },
     });
-
-    console.log('udpated: ', photo);
 
     return Response.json({ data: photo, message: 'Photo updated' });
   } catch (error) {
