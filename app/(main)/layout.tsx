689afc9bede3df7e59ed065fca33df57b4163991
ArlieT <torresarlie22@gmail.com>
2024-09-23 04:31:50 +0800

    chore(test-opengraph image): logo image

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={`${bebasNeue.variable} ${feixenBold.variable} ${feixen.variable} ${oldStandard.variable} ${jetbrains.variable}`}
       >
